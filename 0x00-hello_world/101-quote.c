b133f446e51d9ee2d54c88f98b04abad5ad7d6f4
Mike-obi <okutalukwemichael@gmail.com>
2022-09-08 17:15:23 -0700

    TASK 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,15 +4,9 @@
  * main - Prints a text
  *
  * Return: Always 1 (Success)
- *
  */
-
 int main(void)
-
 {
-
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19", 19);
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 19);
 	return (1);
-
 }
